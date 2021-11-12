9eda054d7bfce6a143b08944e84f7e38ebf322e7
AndrewSukhov <igrok-cs@yandex.ru>
2021-11-12 22:02:12 +0300

    Основы Java. Цикл while.

---
+++ src/basicsJava/whileLoop/task1/Praktikum.java
@@ -0,0 +1,19 @@
+package basicsJava.whileLoop.task1;
+
+import java.util.Scanner;
+
+class Praktikum {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Сколько денег у вас сейчас: ");
+        double balance = scanner.nextDouble(); // Ваши сбережения
+        int years = 0;
+
+        while (...) {
+            balance = ...;
+            years = ...;
+        }
+
+        System.out.println("Через " + ... + " лет у вас будет миллион!");
+    }
+}
