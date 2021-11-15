dab35ca910d41ddefc54b527eae8c5d7588980c5
AndrewSukhov <igrok-cs@yandex.ru>
2021-11-15 23:15:27 +0300

    Основы Java. Массивы. Задача 1.

---
+++ src/basicsJava/arrays/task1/Praktikum.java
@@ -0,0 +1,16 @@
+package basicsJava.arrays.task1;
+
+class Praktikum {
+    public static void main(String[] args) {
+        // Расходы за неделю
+        double mondayExpense = 100.50;
+        double tuesdayExpense = 500.00;
+        double wednesdayExpense = 10.00;
+        double thursdayExpense = 0.0;
+        double fridayExpense = 40.60;
+        double saturdayExpense = 500.10;
+        double sundayExpense = 200.00;
+
+        // Объявите массив expenses и соберите в него значения из переменных
+    }
+}
