80f8b971a4b06eb1f3eb746e5b80aed34af6239c
AndrewSukhov <igrok-cs@yandex.ru>
2021-11-17 16:02:33 +0300

    Основы Java. Учимся работать с массивами: индексы и элементы. Задача 1.

---
+++ src/basicsJava/arrays/indicesAndElements/task1/Praktikum.java
@@ -0,0 +1,16 @@
+package basicsJava.arrays.indicesAndElements.task1;
+
+class Praktikum {
+    public static void main(String[] args) {
+        double[] expenses = {100.50, 500.00, 10.00, 0.0, 640.60, 370.20, 200.00};
+
+        // Добавьте 115 рублей к расходам за среду
+        ...
+        System.out.println("Новое значение расходов за среду: " + ... + " рублей.");
+
+        // Суммируйте три самые крупные траты
+        ...
+        System.out.println("Самые большие расходы были во вторник, пятницу и субботу.");
+        System.out.println("Всего вы потратили в эти дни: " + ... + " рублей.");
+    }
+}
