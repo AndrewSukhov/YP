ff9bd9175d3a7e3a3c3839d37d8d56db370e5400
AndrewSukhov <igrok-cs@yandex.ru>
2021-11-20 23:12:19 +0300

    Основы Java. Массивы. Массивы и циклы. Задача. Решение.

--- src/basicsJava/arrays/ArraysAndLoops/task/Praktikum.java
+++ src/basicsJava/arrays/ArraysAndLoops/task/Praktikum.java
@@ -5,24 +5,28 @@ import java.util.Random;
 class Praktikum {
     public static void main(String[] args) {
         // Объявите пустой массив трат за неделю (7 дней)
-        int[] expenses = ...;
+        int[] expenses = new int[7];
 
         Random random = new Random(); // Генерирует случайное число
 
         // Допишите условие цикла for, чтобы заполнить массив случайными тратами
-        for (int ...; ...; ...) {
+        for (int i = 0; i < expenses.length; i++) {
             expenses[i] = random.nextInt(1000);
         }
 
         System.out.println("Траты за неделю:");
         // Выведите с помощью цикла все траты за неделю в виде: "День ... . Потрачено рублей: ..."
-        ...
+        for (int i = 0; i < expenses.length; i++) {
+            System.out.println("День " + (i + 1) + ". Потрачено рублей: " + expenses[i]);
+        }
 
         int sum = 0;
         // Посчитайте и выведите сумму трат за неделю — используйте цикл и здесь
-        ...
+        for (int expense : expenses) {
+            sum += expense;
+        }
 
-        System.out.println("Траты в рублях за неделю: " + ...);
+        System.out.println("Траты в рублях за неделю: " + sum);
     }
 }
 
