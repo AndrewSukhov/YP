4a6e992f87af10a97ec8a5a99476edfbe7f90e00
AndrewSukhov <igrok-cs@yandex.ru>
2021-11-24 22:05:37 +0300

    Основы Java. Методы. Параметры, аргументы и сигнатура метода. Задача. Решение.

--- src/basicsJava/methods/parametersArgumentsAndMethodSignature/task1/Praktikum.java
+++ src/basicsJava/methods/parametersArgumentsAndMethodSignature/task1/Praktikum.java
@@ -3,14 +3,13 @@ package basicsJava.methods.parametersArgumentsAndMethodSignature.task1;
 public class Praktikum {
 
     public static void main(String[] args) {
-        System.out.println("Наибольшее из чисел 3 и 5 = " + findMax());
+        System.out.println("Наибольшее из чисел 3 и 5 = " + findMax(3,5));
         // Напишите аналогичный вызов метода findMax для таких пар чисел: 16 и 5, -1 и -7
-		...
+        System.out.println("Наибольшее из чисел 16 и 5 = " + findMax(16, 5));
+        System.out.println("Наибольшее из чисел -1 и -7 = " + findMax(-1, -7));
     }
 
-    public static int findMax() {
-        int a = 5;
-        int b = 3;
+    public static int findMax(int a, int b) {
         if (a > b) {
             return a;
         }
