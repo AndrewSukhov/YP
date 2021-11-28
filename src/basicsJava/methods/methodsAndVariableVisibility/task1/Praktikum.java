3a8ea28cf7d0e72d3cb55cf115e7fba80376be3d
AndrewSukhov <igrok-cs@yandex.ru>
2021-11-28 18:58:13 +0300

    Основы Java. Методы. Методы и видимость переменных. Задача 1. Решение.

--- src/basicsJava/methods/methodsAndVariableVisibility/task1/Praktikum.java
+++ src/basicsJava/methods/methodsAndVariableVisibility/task1/Praktikum.java
@@ -5,23 +5,23 @@ public class Praktikum {
 
     public static void main(String[] args) {
 
-        ... // Сохраните общее число мячиков Пикселя в переменной balls
-        System.out.println("У Пикселя " + ... + " мячиков");
+        int balls = 15; // Сохраните общее число мячиков Пикселя в переменной balls
+        System.out.println("У Пикселя " + balls + " мячиков");
 
-		... // Поиграйте с Пикселем, вызвав метод playPixel
+		playPixel(balls); // Поиграйте с Пикселем, вызвав метод playPixel
 
         // После игры Пиксель должен вернуть все мячики на место!
         System.out.println("Пиксель вернул все мячики");
-        System.out.println("Их снова " + ...);
+        System.out.println("Их снова " + balls);
     }
 
     public static void playPixel(int balls) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Сколько мячиков он спрятал?");
-        ... // Сохраните количество мячиков, которые спрятал Пиксель, в переменную hiddenBalls
+        int hiddenBalls = scanner.nextInt(); // Сохраните количество мячиков, которые спрятал Пиксель, в переменную hiddenBalls
 
-        balls = ... // Посчитайте, сколько у Пикселя осталось мячиков
-        System.out.println("Осталось " + ... );
+        balls = balls - hiddenBalls; // Посчитайте, сколько у Пикселя осталось мячиков
+        System.out.println("Осталось " + balls );
     }
 }
 /*
