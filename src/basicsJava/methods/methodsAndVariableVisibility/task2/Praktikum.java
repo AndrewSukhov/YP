99a219671fdf68a861f2434feb2fd479f72520aa
AndrewSukhov <igrok-cs@yandex.ru>
2021-11-28 19:18:02 +0300

    Основы Java. Методы. Методы и видимость переменных. Задача 2. Решение.

--- src/basicsJava/methods/methodsAndVariableVisibility/task2/Praktikum.java
+++ src/basicsJava/methods/methodsAndVariableVisibility/task2/Praktikum.java
@@ -15,41 +15,42 @@ public class Praktikum {
         System.out.println("Какую сумму хотите отложить?");
         int savings = scanner.nextInt();
 
-        ... // Вызовите метод correctExpenses с правильными аргументами
+        correctExpenses(salary, transportMoney, foodMoney, savings); // Вызовите метод correctExpenses с правильными аргументами
 
         // Напечатайте запланированные траты
-        System.out.println("Вы планировали потратить: транспорт — " + ... + ", "
-                + "еда — " + ... + ", "
-                + "сбережения — " + ... + ".");
+        System.out.println("Вы планировали потратить: транспорт — " + transportMoney +", "
+                + "еда — " + foodMoney +", "
+                + "сбережения — " + savings +".");
     }
 
-    ... // Объявите метод correctExpenses
-    // Тело метода дано ниже
+    public static void correctExpenses(int salary, int transportMoney, int foodMoney, int savings) {
+        // Объявите метод correctExpenses
+        // Тело метода дано ниже
 
-    int expensesSum = transportMoney + foodMoney + savings; // Считаем расходы
+        int expensesSum = transportMoney + foodMoney + savings; // Считаем расходы
         if (expensesSum > salary) { // Проверяем, не превышают ли расходы зарплату
-        int lackMoney = expensesSum - salary; // Считаем, сколько не хватает
-        int leftMoney = salary - expensesSum; // Считаем излишек средств
+            int lackMoney = expensesSum - salary; // Считаем, сколько не хватает
 
-        // Пока не начнёт хватать денег на еду — сокращаем траты на 100 рублей
-        while ((salary - foodMoney) < transportMoney) {
-            foodMoney = foodMoney - 100;
-        }
-        // Если не хватает денег на жизнь — не откладываем
-        if (transportMoney + foodMoney + savings > salary) {
-            savings = 0;
-        }
+            // Пока не начнёт хватать денег на еду — сокращаем траты на 100 рублей
+            while ((salary - foodMoney) < transportMoney) {
+                foodMoney = foodMoney - 100;
+            }
+            // Если не хватает денег на жизнь — не откладываем
+            if (transportMoney + foodMoney + savings > salary) {
+                savings = 0;
+            }
 
-        // Печатаем рекомендации
-        System.out.println("Придётся пересмотреть планы, вам не хватает " + lackMoney);
-        System.out.println("Рекомендуемые траты: "
-                + "еда — " + ... + ", "
-                + "сбережения — " + ... + ".");
-    } else {
-        System.out.println("В этом месяце дебет с кредитом сошлись!");
-        System.out.println("Свободных средств " + leftMoney);
+            // Печатаем рекомендации
+            System.out.println("Придётся пересмотреть планы, вам не хватает " + lackMoney);
+            System.out.println("Рекомендуемые траты: "
+                    + "еда — " + foodMoney +", "
+                    + "сбережения — " + savings +".");
+        } else {
+            int leftMoney = salary - expensesSum; // Считаем излишек средств
+            System.out.println("В этом месяце дебет с кредитом сошлись!");
+            System.out.println("Свободных средств " + leftMoney);
+        }
     }
-
 }
 /*
 Условие задачи:
