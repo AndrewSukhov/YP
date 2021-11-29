57b359990cd6b5379a98e3c7a6c21275f3805313
AndrewSukhov <igrok-cs@yandex.ru>
2021-11-29 22:37:45 +0300

    Основы Java. Классы и объекты. Создаём хомяка Задача 2.

---
+++ src/basicsJava/classesAndObjects/createAHamster/task2/Praktikum.java
@@ -0,0 +1,17 @@
+package basicsJava.classesAndObjects.createAHamster.task2;
+
+public class Praktikum {
+    public static void main(String[] args) {
+        Hamster homka = new Hamster();
+
+        System.out.println("Имя: " + homka.name);
+        System.out.println("Возраст: " + ... );
+        System.out.println("Вес: " + ... );
+        System.out.println("Цвет: " + ... );
+    }
+}
+/*
+Условие задачи:
+Хотим знать о хомяке больше: добавьте информацию о нём — возраст, вес и цвет.
+Для обращения к полю нужно использовать точечную нотацию, например, homka.name.
+ */
