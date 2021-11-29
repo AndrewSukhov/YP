f4d9979fac96ba476c0fb545ace02a18b4e6c42a
AndrewSukhov <igrok-cs@yandex.ru>
2021-11-29 22:53:33 +0300

    Основы Java. Классы и объекты. Создаём хомяка Задача 4.

---
+++ src/basicsJava/classesAndObjects/createAHamster/task4/Praktikum.java
@@ -0,0 +1,26 @@
+package basicsJava.classesAndObjects.createAHamster.task4;
+
+public class Praktikum {
+    public static void main(String[] args) {
+        Hamster homka = new Hamster();
+        // Присвойте полям новые значения
+        homka.name = "Хомка";
+        homka.age = 1;
+        homka.weight = 420;
+        homka.color = "Чёрный";
+
+        System.out.println("Имя: " + homka.name);
+        System.out.println("Возраст: " + homka.age);
+        System.out.println("Вес: " +  homka.weight);
+        System.out.println("Цвет: " +  homka.color);
+
+        // Покормите хомяка и напечатайте его вес
+    ...
+        System.out.println("Новый вес: " + ...);
+    }
+}
+/*
+Хомка голодный! Покормите его с помощью метода eat. В качестве аргумента передайте число 15.
+После этого проверьте значение его веса снова, чтобы убедиться, что он поужинал.
+Для обращения к методам тоже используется точка.
+ */
