f3562b0691514e406f9b61bdffa970685e10ae37
AndrewSukhov <igrok-cs@yandex.ru>
2021-11-30 17:46:14 +0300

    Основы Java. Классы и объекты. Создаём хомяка. Конструкторы. Задача 3.

---
+++ src/basicsJava/classesAndObjects/сonstructors/task3/Praktikum.java
@@ -0,0 +1,112 @@
+package basicsJava.classesAndObjects.сonstructors.task3;
+
+import java.util.Scanner;
+
+public class Praktikum {
+    public static void main(String[] args) {
+        double[] expenses = new double[7];
+
+        Scanner scanner = new Scanner(System.in);
+        Converter converter = ...; // Вызовите конструктор с параметрами
+
+        System.out.println("Сколько денег у вас осталось до зарплаты?");
+        double moneyBeforeSalary = scanner.nextDouble();
+
+        System.out.println("Сколько дней до зарплаты?");
+        int daysBeforeSalary = scanner.nextInt();
+
+        while (true) {
+            printMenu();
+            int command = scanner.nextInt();
+
+            if (command == 1) {
+                System.out.println("Ваши сбережения: " + moneyBeforeSalary + " RUB");
+                System.out.println("В какую валюту хотите конвертировать? Доступные варианты: 1 - USD, 2 - EUR, 3 - JPY.");
+                int currency = scanner.nextInt();
+                converter.convert(moneyBeforeSalary, currency);
+            } else if (command == 2) {
+                getAdvice(moneyBeforeSalary, daysBeforeSalary);
+            } else if (command == 3) {
+                moneyBeforeSalary = saveExpense(scanner, moneyBeforeSalary, expenses);
+            } else if (command == 4) {
+                printAllExpenses(expenses);
+            } else if (command == 5) {
+                System.out.println("Самая большая сумма расходов на этой неделе составила " + findMaxExpense(expenses) + " руб.");
+            } else if (command == 0) {
+                System.out.println("Выход");
+                break;
+            } else {
+                System.out.println("Извините, такой команды пока нет.");
+            }
+        }
+    }
+
+    public static double saveExpense(Scanner scanner, double moneyBeforeSalary, double[] expenses) {
+        System.out.println("За какой день вы хотите ввести трату: 1-ПН, 2-ВТ, 3-СР, 4-ЧТ, 5-ПТ, 6-СБ, 7-ВС?");
+        int day = scanner.nextInt();
+        System.out.println("Введите размер траты:");
+        double expense = scanner.nextDouble();
+        moneyBeforeSalary = moneyBeforeSalary - expense;
+        expenses[day - 1] = expenses[day - 1] + expense;
+        System.out.println("Значение сохранено! Ваш текущий баланс в рублях: " + moneyBeforeSalary);
+        if (moneyBeforeSalary < 1000) {
+            System.out.println("На вашем счету осталось совсем немного. Стоит начать экономить!");
+        }
+        return moneyBeforeSalary;
+    }
+
+    public static void printAllExpenses(double[] expenses) {
+        for (int i = 0; i < expenses.length; i++) {
+            System.out.println("День " + (i + 1) + ". Потрачено " + expenses[i] + " рублей");
+        }
+    }
+
+    public static double findMaxExpense(double[] expenses) {
+        double maxExpense = 0;
+        for (int i = 0; i < expenses.length; i++) {
+            if (expenses[i] > maxExpense) {
+                maxExpense = expenses[i];
+            }
+        }
+        return maxExpense;
+    }
+
+    public static void printMenu() {
+        System.out.println("Что вы хотите сделать? ");
+        System.out.println("1 - Конвертировать валюту");
+        System.out.println("2 - Получить совет");
+        System.out.println("3 - Ввести трату");
+        System.out.println("4 - Показать траты за неделю");
+        System.out.println("5 - Показать самую большую сумму расходов за неделю");
+        System.out.println("0 - Выход");
+    }
+
+    public static void getAdvice(double moneyBeforeSalary, int daysBeforeSalary) {
+        if (moneyBeforeSalary < 3000) {
+            System.out.println("Сегодня лучше поесть дома. Экономьте, и вы дотянете до зарплаты!");
+        } else if (moneyBeforeSalary < 10000) {
+            if (daysBeforeSalary < 10) {
+                System.out.println("Окей, пора в Макдак!");
+            } else {
+                System.out.println("Сегодня лучше поесть дома. Экономьте, и вы дотянете до зарплаты!");
+            }
+        } else if (moneyBeforeSalary < 30000) {
+            if (daysBeforeSalary < 10) {
+                System.out.println("Неплохо! Прикупите долларов и зайдите поужинать в классное место. :)");
+            } else {
+                System.out.println("Окей, пора в Макдак!");
+            }
+        } else {
+            if (daysBeforeSalary < 10) {
+                System.out.println("Отлично! Заказывайте крабов!");
+            } else {
+                System.out.println("Неплохо! Прикупите долларов и зайдите поужинать в классное место. :)");
+            }
+        }
+    }
+}
+/*
+Осталось исправить код в классе Praktikum — конструктор по-умолчанию больше не генерируется и возникает ошибка.
+При вызове конструктора с параметрами передайте следующие значения аргуметов: 78.5 для доллара, 88.7 для евро и 0.75 для японской иены.
+Вызовите конструктор new Converter() с правильными значениями аргументов.
+ */
