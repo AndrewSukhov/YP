6b10d529cfe51ac10fd992a4b84ec97bff9a15d6
AndrewSukhov <igrok-cs@yandex.ru>
2021-12-06 22:14:59 +0300

    Java Core I часть.Типы в Java. Значения по умолчанию.

---
+++ src/JavaCore1/module1/sptint1/theme03/task0303/WeatherCalendar.java
@@ -0,0 +1,14 @@
+package JavaCore1.module1.sptint1.theme03.task0303;
+
+class WeatherCalendar {
+    String month;
+    int numberOfDays;
+    int rainyDays;
+    int year;
+    boolean isRainyMonth;
+
+    // Увеличить переменную rainyDays на единицу
+    public void addRainyDay() {
+            ...
+    }
+}
