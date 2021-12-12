cf8c076d18456e0bfda3356510ed224f4792cb88
AndrewSukhov <igrok-cs@yandex.ru>
2021-12-12 19:39:23 +0300

    Java Core I часть.Типы в Java. Парсим строки и не только. Решение.

--- src/JavaCore1/module1/sptint1/theme03/task0306/Praktikum.java
+++ src/JavaCore1/module1/sptint1/theme03/task0306/Praktikum.java
@@ -9,17 +9,17 @@ public class Praktikum {
         String milkKcal = "13.5"; // калорийность молока
 
         float pixelChoice = getMinKcalsSum(beefKcal, chickenKcal, creamKcal, milkKcal);
-        float totalKcal = int.parseInt(pixelKcalDay) + pixelChoice;
+        float totalKcal = Integer.parseInt(pixelKcalDay) + pixelChoice;
         checkKcal(totalKcal);
     }
 
     private static Float getMinKcalsSum(String firstDishKcal, String secondDishKcal, String firstDesert, String secondDesert) {
-        float minDishKcal = ... // вычислите минимальную калорийность основного блюда
-        float minDesertKcal = ... // вычислите минимальную калорийность десерта
+        float minDishKcal = Float.min(Float.parseFloat(firstDishKcal), Float.parseFloat(secondDishKcal)); // вычислите минимальную калорийность основного блюда
+        float minDesertKcal = Float.min(Float.parseFloat(firstDesert), Float.parseFloat(secondDesert)); // вычислите минимальную калорийность десерта
         return minDishKcal + minDesertKcal;
     }
 
-    private static void checkKcal(float catKcal) {
+    private static void checkKcal(Float catKcal) {
         if (catKcal == null) {
             System.out.println("Что-то пошло не так");
         } else {
