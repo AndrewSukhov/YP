3c59af74760904f6846bdc3a71b1a0130da24602
AndrewSukhov <igrok-cs@yandex.ru>
2021-12-19 23:19:19 +0300

    Java Core I часть. Списки и хеш-таблицы. Добавляем элементы в хеш-таблицу.

---
+++ src/JavaCore1/module1/sptint1/theme05/task0506/Praktikum.java
@@ -0,0 +1,24 @@
+package JavaCore1.module1.sptint1.theme05.task0506;
+
+public class Praktikum {
+    public static void main(String[] args) {
+        // System.out.println(stateHolidays)
+    }
+}
+/*
+Напишите код хеш-таблицы государственных праздников stateHolidays,
+которая будет содержать месяцы и праздничные даты:
+Январь — 1, 7;
+Февраль — 23;
+Март — 8;
+Май — 1, 9;
+Июнь — 12;
+Ноябрь — 4.
+
+Импортируйте классы HashMap и ArrayList.
+Ключи хеш-таблицы stateHolidays должны быть строкового типа — String,
+а значения — списки из целых чисел ArrayList<Integer>.
+Создайте для каждого месяца свой список: january, february, march, may, june, november.
+Добавьте в списки элементы с помощью метода add().
+Добавьте элементы в хеш-таблицу с помощью метода put().
+ */
