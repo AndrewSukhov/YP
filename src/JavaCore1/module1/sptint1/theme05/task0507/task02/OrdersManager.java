fe9dacfb6eb4854ae90ddf57ca536bab7a816a85
AndrewSukhov <igrok-cs@yandex.ru>
2021-12-21 11:40:28 +0300

    Java Core I часть. Списки и хеш-таблицы. Операции с хеш-таблицами. Задача 2.

---
+++ src/JavaCore1/module1/sptint1/theme05/task0507/task02/OrdersManager.java
@@ -0,0 +1,92 @@
+package JavaCore1.module1.sptint1.theme05.task0507.task02;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class OrdersManager {
+    HashMap<String, ArrayList<Double>> customersOrders;
+
+    public OrdersManager() {
+        customersOrders = new HashMap<>();
+        ArrayList<Double> orders = new ArrayList<>();
+        orders.add(154.43);
+        orders.add(5453.98);
+        orders.add(8776.65);
+        customersOrders.put("Иван И.", orders);
+
+        orders = new ArrayList<>();
+        orders.add(25343.54);
+        orders.add(420.50);
+        customersOrders.put("Ольга С.", orders);
+
+        orders = new ArrayList<>();
+        orders.add(325.90);
+        customersOrders.put("Александр Т.", orders);
+
+        orders = new ArrayList<>();
+        orders.add(253.54);
+        orders.add(420.50);
+        customersOrders.put("Александр Р.", orders);
+
+        orders = new ArrayList<>();
+        orders.add(780.54);
+        orders.add(420.50);
+        orders.add(36343.54);
+        orders.add(2000.50);
+        customersOrders.put("Екатерина О.", orders);
+    }
+
+    void printAllOrders() {
+        for (String name : ...) { // Цикл должен пройтись по ключам
+            System.out.println("Заказы " + name + ":");
+            ArrayList<Double> value = customersOrders.get(name);
+            System.out.println(value);
+        }
+    }
+
+    double getOrdersSum() {
+        double sum = 0;
+        for (ArrayList<Double> orders : ...) { // Здесь должен быть обход по значениям
+            for (double orderPrice : orders) {
+                sum += orderPrice;
+            }
+        }
+        return sum;
+    }
+
+    void printCustomerOrders(String customerName) {
+        if (...) { // Проверьте, есть ли указанный ключ в таблице
+            System.out.println("Заказы " + customerName + ":");
+            System.out.println(customersOrders.get(customerName));
+        }
+    }
+
+    String getMaxOrderCustomerName() {
+        double maxOrder = 0;
+        String customerName = "";
+
+        ... // Допишите логику работы метода
+
+        return customerName;
+    }
+
+    void removeUnprofitableOrders() {
+        ArrayList<String> names ... // Создайте список клиентов с заказами меньше 5000
+
+        // Наполните список names
+        for ...
+
+        double ordersSum = 0;
+        for ...
+
+        if (ordersSum < 5000) {
+                ...
+        }
+    }
+
+        for ... // Удалите из хеш-таблицы тех, чьи расходы не превышают 5000
+
+                System.out.println("Клиента " + name + " больше нет в таблице.");
+}
+    }
+            }
