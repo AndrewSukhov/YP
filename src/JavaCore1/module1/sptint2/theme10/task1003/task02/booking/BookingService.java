8bda1c8e1b95563e7d258518110e4da84d857a76
AndrewSukhov <igrok-cs@yandex.ru>
2021-12-28 16:30:30 +0300

    Java Core I часть. ООП. Инкапсуляция и наследование. Модификаторы доступа. Задача 2.

---
+++ src/JavaCore1/module1/sptint2/theme10/task1003/task02/booking/BookingService.java
@@ -0,0 +1,55 @@
+package JavaCore1.module1.sptint2.theme10.task1003.task02.booking;
+
+import JavaCore1.module1.sptint2.theme10.task1003.task02.BedAndBreakfast;... // импортируйте классы BedAndBreakfast и Scanner
+
+public class BookingService {
+
+    public static void main(String[] args) {
+        // можно забронировать одну из комнат
+        BedAndBreakfast hotel = new BedAndBreakfast();
+        Scanner scanner = new Scanner(System.in);
+
+        /* Обратите внимание, здесь для удобства мы используем
+        другую конструкцию для оформления цифрового меню.*/
+
+        while (true) {
+            System.out.println("Что вы хотите сделать?");
+            System.out.println("1 - Забронировать номер");
+            System.out.println("2 - Освободить номер");
+            System.out.println("3 - Проверить наличие свободных номеров");
+            System.out.println("0 — Выход");
+
+            int command = scanner.nextInt();
+            /* switch-case конструкция заменяет if-else условия,
+            код получается компактнее и быстрее читается */
+            switch (command) {
+                case 1:
+                    // используется публичный метод
+                    hotel.book();
+                    // break позволяет выйти из условия, иначе - идём дальше
+                    break;
+                case 2:
+                    hotel.free();
+                    break;
+                case 3:
+                    if (hotel.full) {
+                        System.out.println("Больше нет мест¯\\_(ツ)_/¯");
+                    } else {
+                        System.out.println("Есть свободные номера!");
+                    }
+                    break;
+                case 0:
+                    System.out.println("Выход");
+
+                    /* Метод exit() останавливает работу JVM, завершая программу.
+                    В качестве аргумента принимает число — код ошибки.
+                    Если оно не равно 0 — программа завершилась некорректно.*/
+                    System.exit(0); // код равен 0, программа была завершена намеренно
+                    // break в этом случае не нужен
+                default:
+                    System.out.println("Извините, такой команды пока нет.");
+                    break;
+            }
+        }
+    }
+}
