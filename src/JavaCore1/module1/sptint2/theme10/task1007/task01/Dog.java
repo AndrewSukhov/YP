6560554024eea8a4ca52309f4888cf9f3a097ee7
AndrewSukhov <igrok-cs@yandex.ru>
2021-12-29 16:54:33 +0300

    Java Core I часть. ООП. Инкапсуляция и наследование. Переопределяем методы. Решение.

---
+++ src/JavaCore1/module1/sptint2/theme10/task1007/task01/Dog.java
@@ -0,0 +1,8 @@
+package JavaCore1.module1.sptint2.theme10.task1007.task01;
+
+public class Dog extends Animal {
+    @Override
+    public String say() {
+        return "woof";
+    }
+}
--- src/JavaCore1/module1/sptint2/theme10/task1007/task01/Practicum.java
+++ src/JavaCore1/module1/sptint2/theme10/task1007/task01/Practicum.java
@@ -6,12 +6,25 @@ public class Practicum {
         Cat cat = new Cat();
         Bird bird = new Bird();
         Mouse mouse = new Mouse();
-        ... // инициализируйте все нужные объекты
+        // инициализируйте все нужные объекты
+        Cow cow = new Cow();
+        Frog frog = new Frog();
+        Elephant elephant = new Elephant();
+        Duck duck = new Duck();
+        Fish fish = new Fish();
+        Seal seal = new Seal();
+
         System.out.println("Dog goes " + dog.say());
         System.out.println("Cat goes " + cat.say());
         System.out.println("Bird goes " + bird.say());
         System.out.println("And mouse goes " + mouse.say());
-        ... // вызовите переопределённые методы
+        // вызовите переопределённые методы
+        System.out.println("Cow goes " + cow.say());
+        System.out.println("Frog goes " + frog.say());
+        System.out.println("And the elephant goes " + elephant.say());
+        System.out.println("Ducks say " + duck.say());
+        System.out.println("And fish go " + fish.say());
+        System.out.println("And the seal goes " + seal.say());
     }
 }
 /*
