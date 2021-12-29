43c594bd82c64637c6f785e5f9a27eba20970703
AndrewSukhov <igrok-cs@yandex.ru>
2021-12-29 15:55:03 +0300

    Java Core I часть. ООП. Инкапсуляция и наследование. Переопределяем методы.

---
+++ src/JavaCore1/module1/sptint2/theme10/task1007/task01/Animal.java
@@ -0,0 +1,7 @@
+package JavaCore1.module1.sptint2.theme10.task1007.task01;
+
+public class Animal {
+    public String say() {
+        return "I'm an Animal!";
+    }
+}
---
+++ src/JavaCore1/module1/sptint2/theme10/task1007/task01/Practicum.java
@@ -0,0 +1,42 @@
+package JavaCore1.module1.sptint2.theme10.task1007.task01;
+
+public class Practicum {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        Cat cat = new Cat();
+        Bird bird = new Bird();
+        Mouse mouse = new Mouse();
+        ... // инициализируйте все нужные объекты
+        System.out.println("Dog goes " + dog.say());
+        System.out.println("Cat goes " + cat.say());
+        System.out.println("Bird goes " + bird.say());
+        System.out.println("And mouse goes " + mouse.say());
+        ... // вызовите переопределённые методы
+    }
+}
+/*
+В 2013 году норвежский дуэт братьев Илвисокеров Ylvis выпустил на YouTube вирусный клип
+“What Does the Fox Say?” (англ. «Что говорит лиса?») — к настоящему моменту его посмотрели более 1 млрд раз.
+Сюжет песни посвящен тому, что отлично известно, как говорят многие животные:
+собака — «гав», кошка — «мяу», корова — «му», а вот как говорит лиса — великая тайна.
+Немного передохните — послушайте песенку, а потом с помощью наследования и переопределения методов запрограммируйте её первый куплет:
+
+Dog goes woof
+Cat goes meow
+Bird goes tweet
+And mouse goes squeek
+Cow goes moo
+Frog goes croak
+And the elephant goes toot
+Ducks say quack
+And fish go blub
+And the seal goes ow ow ow
+
+Не забудьте использовать аннотацию @Override!
+
+Нужно создать 10 подклассов от Animal с помощью ключевого слова extends.
+Имена подклассов должны совпадать с названиями животных в единственном числе: Dog, Cat, Bird, Mouse и так далее.
+Необходимо переопределить метод say() в каждом подклассе.
+Перед переопределяемым методом нужно поставить аннотацию @Override.
+При переопределении не нужно менять модификатор доступа и сигнатуру метода.
+ */
