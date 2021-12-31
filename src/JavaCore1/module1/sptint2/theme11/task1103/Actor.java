54dceb0e0bc7930f079ce05a13c6b6dd0e232f87
AndrewSukhov <igrok-cs@yandex.ru>
2021-12-31 20:40:11 +0300

    Java Core I часть. Класс Object и его методы. Вычисляем хеш-код через hashCode(). Решение.

--- src/JavaCore1/module1/sptint2/theme11/task1103/Actor.java
+++ src/JavaCore1/module1/sptint2/theme11/task1103/Actor.java
@@ -1,6 +1,26 @@
 package JavaCore1.module1.sptint2.theme11.task1103;
 
+import java.util.Objects;
+
 public class Actor {
     String firstName;
     String lastName;
+
+    public Actor(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Actor)) return false;
+        Actor actor = (Actor) o;
+        return firstName.equals(actor.firstName) && lastName.equals(actor.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName);
+    }
 }
--- src/JavaCore1/module1/sptint2/theme11/task1103/Movie.java
+++ src/JavaCore1/module1/sptint2/theme11/task1103/Movie.java
@@ -1,10 +1,30 @@
 package JavaCore1.module1.sptint2.theme11.task1103;
 
+import java.util.Objects;
+
 public class Movie {
     String title; // название фильма
     int releaseYear; // год выхода на экраны
 
+    public Movie(String title, int releaseYear) {
+        this.title = title;
+        this.releaseYear = releaseYear;
+    }
+
     public String description() { // метод для вывода описания фильма
         return '"' + title + "\" (" + releaseYear + " год)";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Movie)) return false;
+        Movie movie = (Movie) o;
+        return releaseYear == movie.releaseYear && title.equals(movie.title);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, releaseYear);
     }
 }
