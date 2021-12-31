6d8ec152967680108ca07f039ab25a0c5bcdc515
AndrewSukhov <igrok-cs@yandex.ru>
2021-12-31 13:07:31 +0300

    Java Core I часть. Класс Object и его методы. Класс Object — австралопитек в программировании. Решение.

--- src/JavaCore1/module1/sptint2/theme11/task1101/LostAndFoundOffice.java
+++ src/JavaCore1/module1/sptint2/theme11/task1101/LostAndFoundOffice.java
@@ -4,14 +4,28 @@ import java.util.ArrayList;
 
 public class LostAndFoundOffice {
     // создайте список things
+    ArrayList<Object> things;
+
+    public LostAndFoundOffice() {
+        this.things = new ArrayList<>();
+    }
 
     // реализуйте метод put()
+    public void put(Object object) {
+        this.things.add(object);
+    }
 
     // реализуйте метод check()
-
-    for (Object object : things) { // логика проверки вещи в списке
-        if(object.equals(target)) {
-            return true;
+    public boolean check(Object target) {
+        if (target == null) {
+            return false;
+        } else {
+            for (Object object : things) { // логика проверки вещи в списке
+                if (object.equals(target)) {
+                    return true;
+                }
+            }
         }
-
+        return false;
     }
+}
