d73e3482061d5d807c7dbabcd9aafb1f590c75f8
AndrewSukhov <igrok-cs@yandex.ru>
2022-01-08 23:48:18 +0300

    Java Core I часть. Другие модификаторы и работа с перечислениями. Модификатор static. Задача 1.

---
+++ src/JavaCore1/module1/sptint3/theme15/task1501/Forest.java
@@ -0,0 +1,15 @@
+package JavaCore1.module1.sptint3.theme15.task1501;
+
+import java.util.List;
+
+public class Forest {
+    private List<MountainHare> hares;
+
+    // объявите недостающие переменные и добавьте конструктор
+
+    // добавьте метод setSeason(String newSeason)
+    // в этом методе реалищуйте логику смены цвета шубок зайцев-беляков
+
+    // добавьте метод printHares()
+
+}
---
+++ src/JavaCore1/module1/sptint3/theme15/task1501/MountainHare.java
@@ -0,0 +1,16 @@
+package JavaCore1.module1.sptint3.theme15.task1501;
+
+public class MountainHare {
+
+    // добавьте переменные и конструктор
+
+    @Override
+    public String toString() {
+        return "Заяц-беляк: " +
+                "age=" + age +
+                ", weight=" + weight +
+                ", jumpLength=" + jumpLength +
+                ", color=" + color +
+                '.';
+    }
+}
