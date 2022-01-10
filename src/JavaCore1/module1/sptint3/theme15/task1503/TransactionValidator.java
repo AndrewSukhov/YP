535cd8855410f5344af1aff2dd0ffeaadd8be605
AndrewSukhov <igrok-cs@yandex.ru>
2022-01-10 15:32:36 +0300

    Java Core I часть. ООП. Другие модификаторы и работа с перечислениями. Константы. Решение.

--- src/JavaCore1/module1/sptint3/theme15/task1503/TransactionValidator.java
+++ src/JavaCore1/module1/sptint3/theme15/task1503/TransactionValidator.java
@@ -2,9 +2,20 @@ package JavaCore1.module1.sptint3.theme15.task1503;
 
 public class TransactionValidator {
     // объявите константы
+    public static final Double MIN_AMOUNT = 1d;
+    public static final Double MAX_AMOUNT = 5000d;
 
     // объявите метод isValidAmount()
-    // внутри метода добавьте проверки на минимальную и максимальную сумму перевода
-    System.out.println("Минимальная сумма перевода: " + ... + " р. Попробуйте ещё раз!");
-    System.out.println("Максимальная сумма перевода: " + ... + " р. Попробуйте ещё раз!");
+    public static boolean isValidAmount(double amount){
+        // внутри метода добавьте проверки на минимальную и максимальную сумму перевода
+        if (amount < MIN_AMOUNT){
+            System.out.println("Минимальная сумма перевода: " + MIN_AMOUNT + " р. Попробуйте ещё раз!");
+            return false;
+        } else if (amount > MAX_AMOUNT){
+            System.out.println("Максимальная сумма перевода: " + MAX_AMOUNT + " р. Попробуйте ещё раз!");
+            return false;
+        } else {
+            return true;
+        }
+    }
 }
