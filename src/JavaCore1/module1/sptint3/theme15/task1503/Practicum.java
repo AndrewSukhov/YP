95b3407d3593003f8866b6b65964f709d2f56c3c
AndrewSukhov <igrok-cs@yandex.ru>
2022-01-10 14:56:44 +0300

    Java Core I часть. ООП. Другие модификаторы и работа с перечислениями. Константы.

---
+++ src/JavaCore1/module1/sptint3/theme15/task1503/Practicum.java
@@ -0,0 +1,18 @@
+package JavaCore1.module1.sptint3.theme15.task1503;
+
+import java.util.Scanner;
+
+public class Practicum {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Пожалуйста, введите сумму перевода в рублях.");
+        // считайте сумму перевода
+
+        boolean isValid = ... // добавьте вызов метод isValidAmount
+        if (isValid)
+            System.out.println("Спасибо! Ваш перевод на сумму " + amount + " р. успешно выполнен.");
+    }
+}
+}
