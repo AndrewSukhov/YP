464b4b4a700574371550bff8787bbf710c5019a6
AndrewSukhov <igrok-cs@yandex.ru>
2022-02-07 16:45:28 +0300

    Java Core I часть. Коллекции в Java: хеш-таблицы и множества. Реализации LinkedHashMap и TreeMap. Решение.

--- src/JavaCore1/module1/sptint4/theme21/task2103/task01/Practicum.java
+++ src/JavaCore1/module1/sptint4/theme21/task2103/task01/Practicum.java
@@ -1,12 +1,21 @@
 package JavaCore1.module1.sptint4.theme21.task2103.task01;
 
-import java.util.HashMap;
+import java.util.Comparator;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class Practicum {
     public static void main(String[] args) {
+
+        Comparator<Car> carComparator = new Comparator<>() {
+            @Override
+            public int compare(Car car1, Car car2) {
+                return car1.priceInRubles - car2.priceInRubles;
+            }
+        };
+
         // ключ – автомобиль, значение – цена
-        Map<Car, Integer> cars = new HashMap<>();
+        Map<Car, Integer> cars = new TreeMap<>(carComparator);
 
         // хеш-таблица заполняется данными
         cars.put(new Car("Audi A6", 3_760_000), 2);
