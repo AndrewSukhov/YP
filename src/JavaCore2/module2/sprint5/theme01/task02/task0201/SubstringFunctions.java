b7fe13e2cedc6dc4075caed9d3902abd605b486e
AndrewSukhov <igrok-cs@yandex.ru>
2022-02-28 15:37:23 +0300

    Java Core II часть. Вся правда о строках. Подстроки и внутристроковая нумерация.

---
+++ src/JavaCore2/module2/sprint5/theme01/task02/task0201/SubstringFunctions.java
@@ -0,0 +1,16 @@
+package JavaCore2.module2.sprint5.theme01.task02.task0201;
+
+public class SubstringFunctions {
+    public boolean startsWith(String initial, String other) {
+        ... // реализуйте метод
+    }
+}
+/*
+В отделах кадров часто требуется искать работников по первым буквам фамилии.
+От перебора бумажных картотек уже отказались — все персональные данные загружены в электронную базу.
+Вам нужно реализовать метод boolean startsWith(String initial, String other), который возвращает true,
+если фамилия работника initial начинается с подстроки other, иначе false.
+
+Чтобы проверить содержание подстроки в сроке нужно вызвать для initial метод indexOf(), в который передать other.
+Если вызов indexOf() вернёт значение 0, то метод вернёт true, иначе false — это нам и нужно.
+*/
