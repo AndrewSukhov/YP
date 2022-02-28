3f43894cc78fd896fa4afb2f5d98811e366493d3
AndrewSukhov <igrok-cs@yandex.ru>
2022-02-28 19:50:07 +0300

    Java Core II часть. Вся правда о строках. Методы String для поиска и получения подстрок.

---
+++ src/JavaCore2/module2/sprint5/theme01/task0103/task01/Practicum.java
@@ -0,0 +1,9 @@
+package JavaCore2.module2.sprint5.theme01.task0103.task01;
+
+public class Practicum {
+    public static void main(String[] args) {
+        FindRepeats check = new FindRepeats();
+        int count = check.numberOfRepeats("раз, раз, раз", "раз");
+        System.out.println(count);
+    }
+}
