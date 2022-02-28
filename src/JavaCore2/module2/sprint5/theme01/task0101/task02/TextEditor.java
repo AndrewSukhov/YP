cff0e8eb6cbbd313a10a1f93fc8b6cf29f7157d2
AndrewSukhov <igrok-cs@yandex.ru>
2022-02-28 14:34:06 +0300

    Java Core II часть. Вся правда о строках. Класс String.  Задача 2. Решение.

--- src/JavaCore2/module2/sprint5/theme01/task0101/task02/TextEditor.java
+++ src/JavaCore2/module2/sprint5/theme01/task0101/task02/TextEditor.java
@@ -5,11 +5,15 @@ public class TextEditor {
     private boolean isCapsLock = false;
 
     public void capsLock() {
-        // Здесь нужно изменить значение флага isCapsLock на противоположное
+        isCapsLock = !isCapsLock;
     }
 
     public void print(String str) {
-        // А здесь распечатать строку в верхнем или нижнем регистре с учётом флага
+        if (isCapsLock) {
+            System.out.println(str.toUpperCase());
+        } else {
+            System.out.println(str.toLowerCase());
+        }
     }
 }
 /*
