cbbab0ff1bbef737b07df1ce16b937b38f8195bf
AndrewSukhov <igrok-cs@yandex.ru>
2022-03-01 10:20:02 +0300

    Java Core II часть. Вся правда о строках. Методы String для поиска и получения подстрок. Задача 2.

---
+++ src/JavaCore2/module2/sprint5/theme01/task0103/task02/Practicum.java
@@ -0,0 +1,8 @@
+package JavaCore2.module2.sprint5.theme01.task0103.task02;
+
+public class Practicum {
+    public static void main(String[] args) {
+        Capitalizator check = new Capitalizator();
+        System.out.println(check.capitalize("я не волшебник, я только учусь!"));
+    }
+}
