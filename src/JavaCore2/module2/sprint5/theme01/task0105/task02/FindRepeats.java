aa4809f1666bee499c3ff658d2cee7e4f6d0277e
AndrewSukhov <igrok-cs@yandex.ru>
2022-03-02 21:52:53 +0300

    `Java Core II часть. Вся правда о строках. Класс StringBuilder и его методы. Задача 2.

---
+++ src/JavaCore2/module2/sprint5/theme01/task0105/task02/FindRepeats.java
@@ -0,0 +1,26 @@
+package JavaCore2.module2.sprint5.theme01.task0105.task02;
+
+public class FindRepeats {
+    int numberOfRepeats(String text, String substring) {
+        int count = 0;
+        while (text.contains(substring)) {
+            count++;
+            text = text.substring(text.indexOf(substring) + substring.length());
+        }
+        return count;
+    }
+}
+/*
+В уроке мы не раз сделали акцент на том, что StringBuilder позволяет не создавать лишние строки.
+Проверьте это! Оптимизируйте программу по подсчету количества вхождений строки в текст из
+предыдущего урока, чтобы она не создавала лишний «мусор». Подсказка: вместо contains()
+в StringBuilder необходимо пользоваться методом indexOf, а вместо получения подстроки
+можно просто удалять лишние элементы.
+
+Создайте объект StringBuilder через конструктор, принимающий аргумент text.
+После этого замените методы по обработке строки. Вместо text.contains(substring)
+нужно искать индекс substring в StringBuilder. Если он не -1, значит, мы нашли совпадение.
+Если совпадение найдено, нужно отрезать все символы от начала StringBuilder до конца
+найденной строки. Для этого используйте функцию sb.delete(0, ..,).
+Второй аргумент должен быть индексом следующего за повторением элемента.
+*/
