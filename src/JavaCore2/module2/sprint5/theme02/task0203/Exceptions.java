3833d41ea8c7814d12d952f72e79410bede819a3
AndrewSukhov <igrok-cs@yandex.ru>
2022-03-13 23:26:34 +0300

    Java Core II часть. Исключения. Обработка исключений. temp

--- src/JavaCore2/module2/sprint5/theme02/task0203/Exceptions.java
+++ src/JavaCore2/module2/sprint5/theme02/task0203/Exceptions.java
@@ -7,7 +7,17 @@ public class Exceptions {
         System.out.print("Введите целое число => ");
         Scanner scanner = new Scanner(System.in);
         final String inputValue = scanner.next();
-        final int parsedValue = IntegerParser.parseInt(inputValue);
+        try {
+            final int parsedValue = IntegerParser.parseInt(inputValue);
+        } catch (NullStringException | EmptyStringException e) {
+            System.out.println("Введена пустая строка.");
+        } catch (StringNotNumberException e) {
+            System.out.println("Вы ввели не целое число.");
+        } catch (StringIsTooBigNumberException e) {
+            System.out.println("Введённое число слишком большое.");
+        } catch (StringIsTooSmallNumberException e) {
+            System.out.println("Введённое число слишком маленькое.");
+        }
         System.out.println(parsedValue);
     }
 }
