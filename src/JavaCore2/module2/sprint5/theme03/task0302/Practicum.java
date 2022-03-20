3645af718b418625ee4d5816f1513f127d1378d8
AndrewSukhov <igrok-cs@yandex.ru>
2022-03-20 23:58:26 +0300

    Java Core II часть. Работа с файлами. Потоки ввода-вывода. Запись и чтение. Решение.

--- src/JavaCore2/module2/sprint5/theme03/task0302/Practicum.java
+++ src/JavaCore2/module2/sprint5/theme03/task0302/Practicum.java
@@ -15,9 +15,21 @@ public class Practicum {
         BufferedReader br = new BufferedReader(reader);
 
         // читайте файл построчно и сразу обновляйте frequencyMap.
+        while (br.ready()) {
+            String line = br.readLine();
+            if (frequencyMap.containsKey(line)) {
+                frequencyMap.put(line, frequencyMap.get(line) + 1);
+            } else {
+                frequencyMap.put(line, 1);
+            }
+        }
+
+        br.close();
 
         // выведите результат в формате "<буква>: <количество>".
-
+        for (Map.Entry<String, Integer> item : frequencyMap.entrySet()) {
+            System.out.printf("%s: %d\n", item.getKey(), item.getValue());
+        }
     }
 }
 /*
@@ -30,34 +42,3 @@ D — устроить вечеринку в офисе.
 Результаты голосования представлены в файле result.txt.
 Обработайте результат и выведите количество проголосовавших для каждого варианта.
  */
-
-/*
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-
-public class Practicum {
-
-    public static void main(String[] args) throws IOException {
-        Map<String, Integer> frequencyMap = new HashMap<>();
-
-        FileReader reader = new FileReader("result.txt");
-        BufferedReader br = new BufferedReader(reader);
-
-        // читайте файл построчно и сразу обновляйте frequencyMap.
-        while (br.ready()) {
-            String line = br.readLine();
-            frequencyMap.put(line, +1);
-        }
-
-        br.close();
-
-        // выведите результат в формате "<буква>: <количество>".
-        for (Map.Entry<String, Integer> item : frequencyMap.entrySet()){
-            System.out.printf("%s: %d\n", item.getKey(), item.getValue());
-        }
-    }
-}
-*/
