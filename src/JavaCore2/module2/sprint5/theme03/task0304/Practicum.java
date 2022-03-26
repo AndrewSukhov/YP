80fd45f3ece22d4890da58e2103223a9992c2f9d
AndrewSukhov <igrok-cs@yandex.ru>
2022-03-26 15:45:45 +0300

    Java Core II часть. Работа с файлами. Кодировки и Юникод. Решение.

--- src/JavaCore2/module2/sprint5/theme03/task0304/Practicum.java
+++ src/JavaCore2/module2/sprint5/theme03/task0304/Practicum.java
@@ -12,7 +12,7 @@ import java.util.Scanner;
 
 public class Practicum {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Введите количество участников: ");
@@ -23,9 +23,15 @@ public class Practicum {
         // если слов меньше, чем участников, то выведите сообщение:
         // "Недостаточно слов в файле. Добавьте слова и обновите файл."
         // и завершите выполнение программы
+        if (words.size() < playersNumber) {
+            System.out.println("Недостаточно слов в файле. Добавьте слова и обновите файл.");
+            return;
+        }
 
         // воспользуйтесь статическим методом Collections.shuffle(List<?> list),
         // чтобы поменять порядок слов случайным образом
+
+        Collections.shuffle(words);
 
         int wordsNumber = words.size() / playersNumber;
 
@@ -42,11 +48,39 @@ public class Practicum {
     private static List<String> readWordsFromFile(String filename) {
         // добавьте построчное чтение из файла с помощью BufferedReader
         // в случае ошибки выведите сообщение: "Произошла ошибка во время чтения файла."
+        ArrayList<String> strs = new ArrayList<>();
+        try (BufferedReader fileReader = new BufferedReader(new FileReader(filename))) {
+            while (fileReader.ready()) {
+                strs.add(fileReader.readLine());
+            }
+        } catch (IOException e) {
+            System.out.println("Произошла ошибка во время чтения файла.");
+        }
+        return strs;
     }
 
-    private static void writeListToFile(List<String> list, String filename) {
+    private static void writeListToFile(List<String> list, String filename) /*throws IOException */{
         // добавьте запись слов в файл с помощью FileWriter
         // в случае ошибки выведите сообщение: "Произошла ошибка во время записи файла."
+        FileWriter fileWriter = null;
+        try (BufferedReader br = new BufferedReader(new FileReader(filename, StandardCharsets.UTF_8))){
+        while (br.ready()) {
+            String newWord = br.readLine();
+            list.add(newWord);
+        }
+/*
+            fileWriter = new FileWriter(filename);
+            for (String string : list) {
+                fileWriter.write(string);
+                }
+*/
+        } catch (IOException e) {
+            System.out.println("Произошла ошибка во время записи файла.");
+        } /*finally {
+            if (fileWriter != null) {
+                fileWriter.close();
+            }
+            }*/
     }
 }
 /*
