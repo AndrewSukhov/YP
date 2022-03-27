42ee0982b9f666e93c28c815806b15c5e238adf3
AndrewSukhov <igrok-cs@yandex.ru>
2022-03-27 10:52:14 +0300

    Java Core II часть. Функциональный стиль и его инструменты. Функциональный стиль программирования. Решение.

--- src/JavaCore2/module2/sprint6/theme6/task0600/Practicum.java
+++ src/JavaCore2/module2/sprint6/theme6/task0600/Practicum.java
@@ -11,7 +11,12 @@ public class Practicum {
                 "Ева Пинк"
         ));
 
-        Comparator<String> comparator = ...// напишите ваш код здесь.
+        Comparator<String> comparator = new Comparator<>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return o1.split(" ")[1].length() - o2.split(" ")[1].length();
+            }
+        };
 
         Collections.sort(people, comparator);
         System.out.println(people);
