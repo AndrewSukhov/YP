028ce1f84b02aa8029110e17f81a32e9370f237a
AndrewSukhov <igrok-cs@yandex.ru>
2022-04-04 14:14:53 +0300

    Java Core II часть. Время и дата. Unix-время. Класс Instant. Решение.

--- src/JavaCore2/module2/sprint6/theme7/task0701/Practicum.java
+++ src/JavaCore2/module2/sprint6/theme7/task0701/Practicum.java
@@ -2,105 +2,74 @@ package JavaCore2.module2.sprint6.theme7.task0701;
 
 import java.time.Instant;
 import java.util.Scanner;
-
 class Practicum {
-
-    // Вынесите в константы:
-    // Количество миллисекунд в одной секунде
-    public static final int ONE_SECOND_MILLIS = ...;
-    // Количество миллисекунд в одной минуте
-    public static final int ONE_MINUTE_MILLIS = ...
-    // Количество секунд в одном часе
-    public static final int ONE_HOUR_SECONDS = ...
-    // Количество миллисекунд в одном часе
-    public static final int ONE_HOUR_MILLIS = ...
-    // Количество секунд в одном дне
-    public static final int ONE_DAY_SECONDS = ...
-    // Количество миллисекунд в одном дне
-    public static final int ONE_DAY_MILLIS = ...;
-
+    public static final int ONE_SECOND_MILLIS = 1000;
+    public static final int ONE_MINUTE_MILLIS = 60 * 1000;
+    public static final int ONE_HOUR_SECONDS = 60 * 60;
+    public static final int ONE_HOUR_MILLIS = ONE_HOUR_SECONDS * 1000;
+    public static final int ONE_DAY_SECONDS = 60 * 60 * 24;
+    public static final int ONE_DAY_MILLIS = ONE_DAY_SECONDS * 1000;
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         System.out.println("Введите отметку времени начала экзамена (в миллисекундах):");
-        ... examStartMillis = ...
-        // Сконвертируйте введённое значение миллисекунд в экземпляр класса Instant
-        Instant examStart = ...
+        long examStartMillis = scanner.nextLong();
+        Instant examStart = Instant.ofEpochMilli(examStartMillis);
         System.out.println("Введено " + examStart);
-
         System.out.println("Введите отметку текущего времени (в миллисекундах):");
-        ... currentTimeMillis = ...
-        // Сконвертируйте введённое значение миллисекунд в экземпляр класса Instant
-        Instant currentTime = ...
+        long currentTimeMillis = scanner.nextLong();
+        Instant currentTime = Instant.ofEpochMilli(currentTimeMillis);
         System.out.println("Введено " + currentTime);
-
-        // Вычислите время, в которое нужно проснуться
-        Instant timeToWakeUp = ...
-        // Проверьте, что текущее время не позже времени пробуждения
-        if (...) {
+        Instant timeToWakeUp = examStart.minusSeconds(ONE_HOUR_SECONDS);
+        if (currentTime.isAfter(timeToWakeUp)) {
             System.out.println("На экзамен уже не успеть, готовься к пересдаче!");
             return;
         }
-
-        // Проверьте, что до времени пробуждения не больше 24 часов
-        // Вместо [] подставьте вывод в правильном формате
-        if (...) {
-            System.out.printf("Дней до экзамена: []. До экзамена ещё слишком далеко, волноваться не о чем!%n",
-                    calculateDuration(...));
+        if (currentTime.plusSeconds(ONE_DAY_SECONDS).isBefore(examStart)) {
+            System.out.printf("Дней до экзамена: %.0f. До экзамена ещё слишком далеко, волноваться не о чем!%n", calculateDuration(currentTime, examStart, TimeUnits.DAYS));
             return;
         }
-
         System.out.println("Введите желаемую продолжительность сна (в часах):");
-        ... sleepGoalHours = ...
-
-        // Проверьте, что введённое количество часов -- положительное
-        if (...) {
+        int sleepGoalHours = scanner.nextInt();
+        if (sleepGoalHours <= 0) {
             System.out.println("Надо поспать хоть сколько-то!");
             return;
         }
-
-        // Вычислите время, в которое нужно ложиться спать
-        Instant timeToGoToSleep = ...;
-        if (...) {
+        int sleepGoalSeconds = sleepGoalHours * ONE_HOUR_SECONDS;
+        Instant timeToGoToSleep = timeToWakeUp.minusSeconds(sleepGoalSeconds);
+        if (currentTime.isAfter(timeToGoToSleep)) {
             System.out.println("Такое количество времени поспать уже не получится!");
             return;
         }
-
-        // Вместо [] подставьте вывод в правильном формате
-        System.out.printf("На учёбу остаётся: [] мс.%n", calculateDuration(...));
+        System.out.printf("На учёбу остаётся: %.0f мс.%n", calculateDuration(currentTime, timeToGoToSleep, TimeUnits.MILLIS));
         System.out.printf(
-                "Это [] мин. или [] ч.%n",
-                calculateDuration(...),
-        calculateDuration(...)
+                "Это %.2f мин. или %.2f ч.%n",
+                calculateDuration(currentTime, timeToGoToSleep, TimeUnits.MINUTES),
+                calculateDuration(currentTime, timeToGoToSleep, TimeUnits.HOURS)
         );
-        System.out.printf("Ведь проснуться нужно уже через [] ч.%n", calculateDuration(...));
+        System.out.printf("Ведь проснуться нужно уже через %.2f ч.%n", calculateDuration(currentTime, timeToWakeUp, TimeUnits.HOURS));
     }
-
-    // Вычислите продолжительность в указанных единицах времени
-    // Воспользуйтесь константами
     private static double calculateDuration(Instant firstMoment, Instant secondMoment, TimeUnits unit) {
         switch (unit) {
             case MILLIS: {
-                return ...
+                return secondMoment.toEpochMilli() - firstMoment.toEpochMilli();
             }
             case SECONDS: {
-                return ...
+                return (double) (secondMoment.toEpochMilli() - firstMoment.toEpochMilli()) / ONE_SECOND_MILLIS;
             }
             case MINUTES: {
-                return ...
+                return (double) (secondMoment.toEpochMilli() - firstMoment.toEpochMilli()) / ONE_MINUTE_MILLIS;
             }
             case HOURS: {
-                return ...
+                return (double) (secondMoment.toEpochMilli() - firstMoment.toEpochMilli()) / ONE_HOUR_MILLIS;
             }
             case DAYS: {
-                return ...
+                return (double) (secondMoment.toEpochMilli() - firstMoment.toEpochMilli()) / ONE_DAY_MILLIS;
             }
             default:
                 return 0;
         }
     }
 }
-
 enum TimeUnits {
     MILLIS,
     SECONDS,
