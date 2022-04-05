32e80b65a609ce3afebe62acb1464aa4442f0de5
AndrewSukhov <igrok-cs@yandex.ru>
2022-04-05 09:05:51 +0300

    Java Core II часть. Unit-тесты. Классы эквивалентности и граничные значения. Решение.

--- src/JavaCore2/module2/sprint6/theme8/task0805/PriceCalculatorTest.java
+++ src/JavaCore2/module2/sprint6/theme8/task0805/PriceCalculatorTest.java
@@ -1,27 +1,77 @@
 package JavaCore2.module2.sprint6.theme8.task0805;
 
-import org.junit.jupiter.api.Assertions;
+/*import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.function.Executable;
 
 public class PriceCalculatorTest {
-
     private final PriceCalculator priceCalculator = new PriceCalculator();
-
     @Test
     public void shouldThrowExceptionWhenBikeAndDistanceIs0Km() {
         IllegalArgumentException ex = Assertions.assertThrows(
                 IllegalArgumentException.class,
                 generateExecutable(TransportType.BIKE, 0)
         );
-
         Assertions.assertEquals("Distance should be more than 0 km", ex.getMessage());
     }
-
+    @Test
+    public void shouldThrowExceptionWhenBikeAndDistanceIsMoreThan20km() {
+        IllegalArgumentException ex = Assertions.assertThrows(
+                IllegalArgumentException.class,
+                generateExecutable(TransportType.BIKE, 21)
+        );
+        Assertions.assertEquals("Bike can not go for more than 20 km", ex.getMessage());
+    }
+    @Test
+    public void shouldReturn200ForBikeAndDistanceIs20() {
+        Assertions.assertEquals(priceCalculator.calculatePrice(TransportType.BIKE, 20), 200);
+    }
+    @Test
+    public void shouldThrowExceptionWhenCarAndDistanceIs0Km() {
+        IllegalArgumentException ex = Assertions.assertThrows(
+                IllegalArgumentException.class,
+                generateExecutable(TransportType.CAR, 0)
+        );
+        Assertions.assertEquals("Distance should be more than 0 km", ex.getMessage());
+    }
+    @Test
+    public void shouldThrowExceptionWhenCarAndDistanceIsMoreThan1000km() {
+        IllegalArgumentException ex = Assertions.assertThrows(
+                IllegalArgumentException.class,
+                generateExecutable(TransportType.CAR, 1001)
+        );
+        Assertions.assertEquals("Car can not go for more than 1000 km", ex.getMessage());
+    }
+    @Test
+    public void shouldReturn7000ForCarAndDistanceIs1000() {
+        Assertions.assertEquals(priceCalculator.calculatePrice(TransportType.CAR, 1000), 7000);
+    }
+    @Test
+    public void shouldThrowExceptionWhenTruckAndDistanceIs0Km() {
+        IllegalArgumentException ex = Assertions.assertThrows(
+                IllegalArgumentException.class,
+                generateExecutable(TransportType.TRUCK, 0)
+        );
+        Assertions.assertEquals("Distance should be more than 0 km", ex.getMessage());
+    }
+    @Test
+    public void shouldReturn5000ForTruckAndDistanceIs1000() {
+        Assertions.assertEquals(priceCalculator.calculatePrice(TransportType.TRUCK, 1000), 5000);
+    }
+    @Test
+    public void shouldThrowExceptionWhenDroneDeliveryIsRequested() {
+        UnsupportedOperationException ex = Assertions.assertThrows(
+                UnsupportedOperationException.class,
+                generateExecutable(TransportType.DRONE, 1)
+        );
+        Assertions.assertEquals(
+                "transport type 'DRONE' is not handled correctly",
+                ex.getMessage());
+    }
     private Executable generateExecutable(TransportType type, int distance) {
         return () -> priceCalculator.calculatePrice(type, distance);
     }
-}
+}*/
 /*
 Вам нужно доработать тесты для класса PriceCalculator по расчёту стоимости доставки
 в зависимости от расстояния и типа транспорта.
