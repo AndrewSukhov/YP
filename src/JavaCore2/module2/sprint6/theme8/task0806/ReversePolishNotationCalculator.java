14bb5d1e9229f11f32873bd30378e2a08b8494dd
AndrewSukhov <igrok-cs@yandex.ru>
2022-04-05 09:09:46 +0300

    Java Core II часть. Unit-тесты. Покрытие кода и покрытие требований.

---
+++ src/JavaCore2/module2/sprint6/theme8/task0806/ReversePolishNotationCalculator.java
@@ -0,0 +1,51 @@
+package JavaCore2.module2.sprint6.theme8.task0806;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class ReversePolishNotationCalculator {
+
+    public int calculatePolishNotation(String str) {
+        String[] parts = str.split(" ");
+        Deque<Integer> numbers = new ArrayDeque<>();
+        int index = 0;
+
+        while (index != parts.length) {
+
+            if (parts[index].isBlank()) {
+                index++;
+                continue;
+            }
+
+            if (isOperation(parts[index])) {
+                int operandOne = numbers.pop();
+                int operandTwo = numbers.pop();
+
+                if (parts[index].equals("+")) {
+                    numbers.push(operandOne + operandTwo);
+                } else if (parts[index].equals("-")) {
+                    numbers.push(operandTwo - operandOne);
+                } else if (parts[index].equals("*")) {
+                    numbers.push(operandOne * operandTwo);
+                }
+
+            } else {
+                numbers.push(Integer.parseInt(parts[index]));
+            }
+
+            index++;
+        }
+
+        return numbers.pop();
+    }
+
+    private boolean isOperation(String part) {
+        if (part.equals("+")
+                || part.equals("-")
+                || part.equals("*")) {
+            return true;
+        }
+
+        return false;
+    }
+}
