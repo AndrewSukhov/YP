7f79e91c5a9e1558be5f1a677b95713dc820eb07
AndrewSukhov <igrok-cs@yandex.ru>
2022-04-22 23:58:08 +0300

    Java Core II часть. Создание API. Публикуем эндпоинты на сервере. Решение.

--- src/JavaCore2/module2/sprint6/theme11/task1102/Practicum.java
+++ src/JavaCore2/module2/sprint6/theme11/task1102/Practicum.java
@@ -11,65 +11,6 @@ import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.util.Random;
 
-public class Practicum {
-    private static final int PORT = 8080;
-
-    // IOException могут сгенерировать методы create() и bind(...)
-    public static void main(String[] args) throws IOException {
-        HttpServer httpServer = HttpServer.create();
-
-        httpServer.bind(new InetSocketAddress(PORT), 0);
-        httpServer.createContext("/hello", new HelloHandler());
-        // добавьте новый обработчик для /day тут
-        httpServer.start(); // запускаем сервер
-
-        System.out.println("HTTP-сервер запущен на " + PORT + " порту!");
-        httpServer.stop(1);
-    }
-
-    static class HelloHandler implements HttpHandler {
-        @Override
-        public void handle(HttpExchange httpExchange) throws IOException {
-            System.out.println("Началась обработка /hello запроса от клиента.");
-
-            String response = "Hey! Glad to see you on our server.";
-            httpExchange.sendResponseHeaders(200, 0);
-
-            try (OutputStream os = httpExchange.getResponseBody()) {
-                os.write(response.getBytes());
-            }
-        }
-    }
-
-    // объявите класс-обработчик тут
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-import com.sun.net.httpserver.HttpExchange;
-import com.sun.net.httpserver.HttpHandler;
-import com.sun.net.httpserver.HttpServer;
-
-import java.io.IOException;
-import java.io.OutputStream;
-import java.net.InetSocketAddress;
-import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
-import java.util.Random;
-
 
 public class Practicum {
     private static final int PORT = 8080;
