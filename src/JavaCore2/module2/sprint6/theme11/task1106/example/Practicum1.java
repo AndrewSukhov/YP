328ecf416c5dbf77b15a4fec00ba604b84a3d696
AndrewSukhov <igrok-cs@yandex.ru>
2022-05-03 16:57:51 +0300

    Java Core II часть. Создание API. httpServer: getRequestBody(), getRequestMethod()

---
+++ src/JavaCore2/module2/sprint6/theme11/task1106/example/Practicum1.java
@@ -0,0 +1,52 @@
+package JavaCore2.module2.sprint6.theme11.task1106.example;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
+public class Practicum1 {
+    private static final int PORT = 8080;
+
+    public static void main(String[] args) throws IOException {
+        HttpServer httpServer = HttpServer.create();
+
+        httpServer.bind(new InetSocketAddress(PORT), 0);
+        httpServer.createContext("/hello", new HelloHandler());
+        httpServer.start();
+
+        System.out.println("HTTP-сервер запущен на " + PORT + " порту!");
+    }
+
+    static class HelloHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange httpExchange) throws IOException {
+
+            String method = httpExchange.getRequestMethod();
+            System.out.println("Началась обработка " + method + " /hello запроса от клиента.");
+
+            String response;
+            switch(method) {
+                case "POST":
+                    response = "Вы использовали метод POST!";
+                    break;
+                case "GET":
+                    response = "Вы использовали метод GET!";
+                    break;
+                default:
+                    response = "Вы использовали какой-то другой метод!";
+            }
+
+            httpExchange.sendResponseHeaders(200, 0);
+
+            try (OutputStream os = httpExchange.getResponseBody()) {
+                os.write(response.getBytes());
+            }
+        }
+    }
+}
