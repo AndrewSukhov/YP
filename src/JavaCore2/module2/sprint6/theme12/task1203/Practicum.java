05caeb810e8467759c63f6f42d0c41df36b360f1
AndrewSukhov <igrok-cs@yandex.ru>
2022-06-10 00:32:22 +0300

    Java Core II часть. Сетевые запросы. Добавляем параметры строки запроса и HTTP-заголовки. Решение.

--- src/JavaCore2/module2/sprint6/theme12/task1203/Practicum.java
+++ src/JavaCore2/module2/sprint6/theme12/task1203/Practicum.java
@@ -5,18 +5,19 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-
 public class Practicum {
-
     public static void main(String[] args) {
         HttpClient client = HttpClient.newHttpClient();
-
         // укажите URL запроса, включая его параметры
-        URI url = ...;
-
+        URI url = URI.create("https://api.exchangerate.host/convert?base=RUB&symbols=USD,EUR");
         // создайте объект, описывающий запрос с необходимой информацией
-        HttpRequest request = ...;
-
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(url)
+                .header("Accept", "application/json")
+                .GET()
+                //.base(RUB)
+                //.symbols(USD,EUR)
+                .build();
         try {
             HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
             System.out.println("Код статуса: " + response.statusCode());
